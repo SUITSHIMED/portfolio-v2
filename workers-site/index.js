3dfc0b196f443a8d55a427fd638a30a4a7979196
lakhrouf mohamed <mohamedlakhrouf@gmail.com>
2026-08-02 15:41:24 +0100

    fixing detail pages

--- workers-site/index.js
+++ workers-site/index.js
@@ -8,6 +8,16 @@ async function handleEvent(event) {
   try {
     return await getAssetFromKV(event);
   } catch (e) {
+    const url = new URL(event.request.url);
+    const isSpaRoute =
+      event.request.method === "GET" && !url.pathname.includes(".");
+
+    if (isSpaRoute) {
+      return await getAssetFromKV(
+        new Request(`${url.origin}/index.html`, event.request)
+      );
+    }
+
     return new Response("Not found", { status: 404 });
   }
 }
